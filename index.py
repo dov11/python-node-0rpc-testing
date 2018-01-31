9e2aacd4e3f03812ae37bd221fc9730f4a5e3f27
dov11 <ovo.dan@gmail.com>
2018-01-31 11:35:16 +0100

    add 'routes' to python server

--- index.py
+++ index.py
@@ -4,6 +4,9 @@ class HelloRPC(object):
     def hello(self, name):
         print("here")
         return "Hello, %s" % name
+    def bye(self, name):
+        print('bye')
+        return "Bye, %s" %name
 
 s = zerorpc.Server(HelloRPC())
 s.bind("tcp://0.0.0.0:4242")
--- routes/hello.js
+++ routes/hello.js
@@ -7,7 +7,7 @@ client.connect("tcp://127.0.0.1:4242");
 router.get('/:name', (req, res, next) => {
     const name = req.params.name
     console.log(name)
-    client.invoke("hello", name, function(error, res, more) {
+    client.invoke("bye", name, function(error, res, more) {
         console.log(res)
     });
   })
